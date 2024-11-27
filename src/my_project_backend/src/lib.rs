b2e1c2d81771b0fc736bd581f9f970d9f3e4dd00
Szymon Mielecki <szymon.mielecki@gmail.com>
2024-11-27 10:02:32 +0000

    Add message storage functionality with save_msg and get_msg methods

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,22 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static MSG: RefCell<String> = RefCell::new(String::from("Hello, world!"));
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
+}
+
+#[ic_cdk::update]
+fn save_msg(msg: String) {
+    MSG.with(|m| {
+        *m.borrow_mut() = msg;
+    });
+}
+
+#[ic_cdk::query]
+fn get_msg() -> String {
+    MSG.with(|m| m.borrow().clone())
 }
